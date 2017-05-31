977c2db3c5559c3dcc2ec4088530f2fdd5ccf9af
FZahorec <frant.zahorec@gmail.com>
2017-05-31 21:47:07 +0200

    úprava proti podvádění ze středy 31. 5. 2017

--- piskvorky.py
+++ piskvorky.py
@@ -1,8 +1,9 @@
 """
 Šachovnice je 2D pole.
-Proto si ji zobrazíme seznam obsahující řádky.
+Proto si ji zobrazíme jako seznam obsahující řádky.
 Každý řádek obsahuje políčka odpovídající sloupcům.
 """
+
 def vytvor_sachovnici():
     sachovnice = []
     for radek in range(3):
@@ -18,24 +19,31 @@ def zobraz_plochu(plocha):
         print(radek)
 #zobraz_plochu(hraci_plocha)
 
+def zadej_vstup(pole, hrac):
+    print("Na tahu je hráč {}.".format(hrac))
+    vstup = True
+    while vstup:
+        radek = int(input(" Zadej číslo řádku. ")) - 1
+        sloupec = int(input("Zadej číslo sloupce. ")) - 1
+        if 0 <= radek < len(pole[0]) and 0 <= sloupec < len(pole):
+            if pole[radek][sloupec].upper() not in ["X","O"]:
+                vstup = False
+                souradnice = [radek, sloupec]
+            else:
+                print("Políčko je obsazené.")
+        else:
+            print("Souřadnice mimo rozsah.")
+    return souradnice
+    
 # Hra - 1. a 2. hráč
 def hra():
     plocha = vytvor_sachovnici()
-    hrac1 = True
-    for i in range(9):
+    hraci = ["X","O"]
+    for tah in range(9):
         zobraz_plochu(plocha)
-        if hrac1 == True:
-            print("Hraje 1. hráč.")
-            radek = int(input("Zadej číslo řádku."))
-            policko = int(input("Zadej číslo sloupce."))
-            plocha[radek-1][policko-1] = "X"
-            hrac1 = False
-        else:
-            print("Hraje 2. hráč.")
-            radek = int(input("Zadej číslo řádku."))
-            policko = int(input("Zadej číslo sloupce."))
-            plocha[radek-1][policko-1] = "O"
-            hrac1 = True
+        hrac = hraci[tah % 2]
+        souradnice = zadej_vstup(plocha, hrac)
+        plocha[souradnice[0]][souradnice[1]] = hrac
     zobraz_plochu(plocha)
 
 hra()
